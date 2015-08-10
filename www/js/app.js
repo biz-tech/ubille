b287b180db36ba07975a39948b795456b64b262a
Patrick Woo <info@biztechus.com>
2015-08-10 13:01:04 -0700

    working controllers.js & services.js

--- www/js/app.js
+++ www/js/app.js
@@ -1,9 +1,27 @@
-var path = "http://crm.biztechus.com";
-var test = "http://172.20.10.205/vtigercrm";
-var params = location.search.split('&')[1]; // list 화면에서 클릭한 값을 담아서 detail 조회할때 사용.
+// Ionic Starter App
 
-angular.module('ionicApp', ['ionic'])
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'starter.services' is found in services.js
+// 'starter.controllers' is found in controllers.js
+angular.module('ubille', ['ionic', 'ubille.controllers', 'ubille.services'])
 
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleLightContent();
+    }
+  });
+})
 .config(function($stateProvider, $urlRouterProvider) {
 
   $stateProvider
@@ -64,11 +82,11 @@ angular.module('ionicApp', ['ionic'])
         }
       }
     })
-    .state('tabs.customer', {
-      url: "/customer",
+    .state('tabs.customers', {
+      url: "/customers",
       views: {
-        'customer-tab': {
-          templateUrl: "templates/customer.html"		  	  
+        'customers-tab': {
+          templateUrl: "templates/customers.html"		  	  
         }
       }
     })
@@ -80,11 +98,11 @@ angular.module('ionicApp', ['ionic'])
         }
       }
     })
-	.state('tabs.accDetail', {
-      url: "/accDetail",
+	.state('tabs.customer-detail', {
+      url: "/customer/:customerId",
       views: {
         'customer-tab': {
-          templateUrl: "templates/accDetail.html"
+          templateUrl: "templates/customer-detail.html"
         }
       }
     })
@@ -92,45 +110,5 @@ angular.module('ionicApp', ['ionic'])
 
    $urlRouterProvider.otherwise("/tab/home");
 
-})
-.controller('NavCtrl', function($scope, $ionicSideMenuDelegate) {
-  $scope.showMenu = function () {
-    $ionicSideMenuDelegate.toggleLeft();
-  };
-  $scope.showRightMenu = function () {
-    $ionicSideMenuDelegate.toggleRight();
-  };
-})
-.controller('HomeTabCtrl', function($scope) {
-})
-.controller('orderCtrl', function($scope, $http) {
-var site= test+"/testdata.php?oper=order";		
-	$http.get(site).success(function(response){
-		$scope.data = response;		
-	});
-})
-.controller('productCtrl', function($scope, $http) {
-//var site= path+"/testdata.php?oper=product";
-	var site= test+"/testdata.php?oper=product";		
-	$http.get(site).success(function(response){
-		$scope.data = response;
-	});
-})
-.controller('accountCtrl', function($scope, $http) {
-	//var site= path+"/testdata.php?oper=acoount";
-	var site= path+"/testdata.php?oper=acoount";	
-	$http.get(site).success(function(response){	
-		$scope.data = response;			
-	});	
-})
-.controller('accDetailCtrl', function($scope, $http) {
-	//var site= path+"/testdata.php?oper=accountDetail&"+params;	
-	var site= test+"/testdata.php?oper=accountDetail&"+params;	
-	$http.get(site).success(function(response){	
-		$scope.data = response;				
-		$scope.movePage = function(){
-			$scope
-		}
-	}); 
-})
-; 
+});
+
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,5 +1,26 @@
-angular.module('ionicApp.controllers', [])
+angular.module('ubille.controllers', [])
 
+.controller('DashCtrl', function($scope) {})
+
+.controller('CustomersCtrl', function($scope, Customers) {
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+  console.log("customer ctrl");
+  customers = Customers.all();
+  console.log(customers);
+  
+  $scope.remove = function(customer) {
+    Customers.remove(customer);
+  };
+})
+.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+  $scope.chat = Chats.get($stateParams.chatId);
+})
 .controller('NavCtrl', function($scope, $ionicSideMenuDelegate) {
   $scope.showMenu = function () {
     $ionicSideMenuDelegate.toggleLeft();
@@ -7,14 +28,12 @@ angular.module('ionicApp.controllers', [])
   $scope.showRightMenu = function () {
     $ionicSideMenuDelegate.toggleRight();
   };
- 
 })
-
-
 .controller('HomeTabCtrl', function($scope) {
-
 })
 
-
-
-
+.controller('AccountCtrl', function($scope) {
+  $scope.settings = {
+    enableFriends: true
+  };
+});
